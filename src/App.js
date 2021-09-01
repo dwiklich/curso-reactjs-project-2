d4f2e63602148a21e259bbd2d9ca720fef606d8a
dwiklich <d.wiklich@gmail.com>
2021-09-01 15:55:00 -0300

    useEffect - React Hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   // const [reverse, setReverse] = useState(false);
   const [counter, setCounter] = useState(0);
   const [counter2, setCounter2] = useState(0);
 
+  const eventFn = () => {
+    console.log('h1 clicado');
+  };
+
+  // componentDidUpdate - executa toda vez que o component atualiza
+  // useEffect(() => {
+  //   console.log('componentDidUpdate');
+  // });
+
+  // componentDidMount - executa 1x
+  useEffect(() => {
+    document.querySelector('h1')?.addEventListener('click', eventFn);
+
+    // componentWillUmount - limpeza
+    return () => {
+      document.querySelector('h1')?.removeEventListener('click', eventFn);
+    };
+  }, []);
+
+  // Com dependência - executa toda vez que a dependência mudar
+  useEffect(() => {
+    console.log('C1:', counter, 'C2:', counter2);
+  }, [counter, counter2]);
+
   return (
     <div className="App">
       <div className="container">
-        <p>conter1 : {counter}</p>
+        <h1>conter1 : {counter}</h1>
         <p>conter2 : {counter2}</p>
 
         <button
