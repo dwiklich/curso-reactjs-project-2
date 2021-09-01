d39db7f7a4882e77f4ea6c7b241ec967b4e53f0c
dwiklich <d.wiklich@gmail.com>
2021-09-01 17:43:50 -0300

    useCallback - React Hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import P from 'prop-types';
+
 import './App.css';
-import { useEffect, useState } from 'react';
+
+const Button = React.memo(function Button({ onClick }) {
+  console.log('Filho, renderizou');
+  return <button onClick={() => onClick(10)}>[ + ]</button>;
+});
+
+Button.propTypes = {
+  onClick: P.func.isRequired,
+  numeroRandom: P.number,
+};
+// const numeroRandom = {
+//   _payload: {
+//     _result: {
+//       number: Number(Math.random().toFixed(2) * (1000 - 100) + 100),
+//     },
+//   },
+// };
 
 function App() {
   // const [reverse, setReverse] = useState(false);
   const [counter, setCounter] = useState(0);
   const [counter2, setCounter2] = useState(0);
+
+  console.log('Pai, renderizou');
+
+  // console.log(numeroRandom._result.call(this.number, ob));
+  // const incrementCounter = (num) => {
+  //   setCounter2((prevState) => prevState + num);
+  //   console.log('Function foi criada', numeroRandom);
+  // };
+
+  // useCallback - mantem a function incrementCounter, sem re-renderizar ela apos o render de outros componentes
+
+  const incrementCounter = useCallback((num) => {
+    setCounter2((prevState) => prevState + num);
+  }, []);
 
   const eventFn = () => {
     console.log('h1 clicado');
@@ -16,6 +49,7 @@ function App() {
   // });
 
   // componentDidMount - executa 1x
+
   useEffect(() => {
     document.querySelector('h1')?.addEventListener('click', eventFn);
 
@@ -35,7 +69,10 @@ function App() {
       <div className="container">
         <h1>conter1 : {counter}</h1>
         <p>conter2 : {counter2}</p>
-
+        <div>
+          useCallback Button
+          <Button onClick={incrementCounter} />
+        </div>
         <button
           onClick={() => {
             setCounter((prevReverse) => prevReverse + 1);
