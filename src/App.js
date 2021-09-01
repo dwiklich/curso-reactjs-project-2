fd01d5ab9323809666f2440464b672a724a7cad7
dwiklich <d.wiklich@gmail.com>
2021-09-01 19:06:15 -0300

    useRef - React Hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import P from 'prop-types';
-import { useEffect, useMemo, useState } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import './App.css';
 
-const Post = ({ post }) => {
+const Post = ({ post, onClick }) => {
   console.log('Filho renderizou');
   return (
     <div key={post.id} className="post">
-      <h1>{post.title}</h1>
+      <h1
+        onClick={() => {
+          onClick(post.title);
+        }}
+      >
+        {post.title}
+      </h1>
       <p>{post.body}</p>
     </div>
   );
@@ -18,27 +24,40 @@ Post.propTypes = {
     title: P.string,
     body: P.string,
   }),
+  onClick: P.func.isRequired,
 };
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [value, setValue] = useState('');
 
+  // useRef - seta o valor o elemento do document.DOM, para manipula-los.
+  const input = useRef(null);
+
   console.log('Pai renderizou!');
 
   // Component did mount
   useEffect(() => {
-    setTimeout(function () {
-      fetch('https://jsonplaceholder.typicode.com/posts')
-        .then((r) => r.json())
-        .then((r) => setPosts(r));
-    }, 5000);
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((r) => r.json())
+      .then((r) => setPosts(r));
   }, []);
+
+  const handleRef = useCallback((value) => {
+    setValue(value);
+  }, []);
+
+  useEffect(() => {
+    // Manipulando o elemento da DOM
+    input.current.focus();
+    console.log(input.current);
+  }, [value]);
 
   return (
     <div className="App">
       <p>
-        <input type="search" value={value} onChange={(e) => setValue(e.target.value)} />
+        {/* ref={} seta o elemento da DOM, */}
+        <input ref={input} type="search" value={value} onChange={(e) => setValue(e.target.value)} />
       </p>
 
       {/* useMemo - memoriza componentes, sem re-renderizar caso o pai atualize, porem o compomente do useMemo renderiza se as dependencias mudarem */}
@@ -46,10 +65,10 @@ function App() {
         return (
           posts.length > 0 &&
           posts.map((post) => {
-            return <Post key={post.id} post={post} />;
+            return <Post key={post.id} post={post} onClick={handleRef} />;
           })
         );
-      }, [posts])}
+      }, [posts, handleRef])}
       {posts.length <= 0 && <p>Ainda não existem posts.</p>}
     </div>
   );
