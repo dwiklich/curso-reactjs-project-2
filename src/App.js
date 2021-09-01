9de4c50cbd90648dfa3b7439a68b5cfe7760c0f8
dwiklich <d.wiklich@gmail.com>
2021-09-01 18:29:21 -0300

    useMemo - React Hook

--- src/App.js
+++ src/App.js
@@ -1,93 +1,56 @@
-import React, { useCallback, useEffect, useState } from 'react';
 import P from 'prop-types';
-
+import { useEffect, useMemo, useState } from 'react';
 import './App.css';
 
-const Button = React.memo(function Button({ onClick }) {
-  console.log('Filho, renderizou');
-  return <button onClick={() => onClick(10)}>[ + ]</button>;
-});
-
-Button.propTypes = {
-  onClick: P.func.isRequired,
-  numeroRandom: P.number,
+const Post = ({ post }) => {
+  console.log('Filho renderizou');
+  return (
+    <div key={post.id} className="post">
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  );
 };
-// const numeroRandom = {
-//   _payload: {
-//     _result: {
-//       number: Number(Math.random().toFixed(2) * (1000 - 100) + 100),
-//     },
-//   },
-// };
+
+Post.propTypes = {
+  post: P.shape({
+    id: P.number,
+    title: P.string,
+    body: P.string,
+  }),
+};
 
 function App() {
-  // const [reverse, setReverse] = useState(false);
-  const [counter, setCounter] = useState(0);
-  const [counter2, setCounter2] = useState(0);
+  const [posts, setPosts] = useState([]);
+  const [value, setValue] = useState('');
 
-  console.log('Pai, renderizou');
+  console.log('Pai renderizou!');
 
-  // console.log(numeroRandom._result.call(this.number, ob));
-  // const incrementCounter = (num) => {
-  //   setCounter2((prevState) => prevState + num);
-  //   console.log('Function foi criada', numeroRandom);
-  // };
-
-  // useCallback - mantem a function incrementCounter, sem re-renderizar ela apos o render de outros componentes
-
-  const incrementCounter = useCallback((num) => {
-    setCounter2((prevState) => prevState + num);
-  }, []);
-
-  const eventFn = () => {
-    console.log('h1 clicado');
-  };
-
-  // componentDidUpdate - executa toda vez que o component atualiza
-  // useEffect(() => {
-  //   console.log('componentDidUpdate');
-  // });
-
-  // componentDidMount - executa 1x
-
+  // Component did mount
   useEffect(() => {
-    document.querySelector('h1')?.addEventListener('click', eventFn);
-
-    // componentWillUmount - limpeza
-    return () => {
-      document.querySelector('h1')?.removeEventListener('click', eventFn);
-    };
+    setTimeout(function () {
+      fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((r) => r.json())
+        .then((r) => setPosts(r));
+    }, 5000);
   }, []);
-
-  // Com dependência - executa toda vez que a dependência mudar
-  useEffect(() => {
-    console.log('C1:', counter, 'C2:', counter2);
-  }, [counter, counter2]);
 
   return (
     <div className="App">
-      <div className="container">
-        <h1>conter1 : {counter}</h1>
-        <p>conter2 : {counter2}</p>
-        <div>
-          useCallback Button
-          <Button onClick={incrementCounter} />
-        </div>
-        <button
-          onClick={() => {
-            setCounter((prevReverse) => prevReverse + 1);
-          }}
-        >
-          +
-        </button>
-        <button
-          onClick={() => {
-            setCounter2((prevReverse) => prevReverse + 1);
-          }}
-        >
-          + (2)
-        </button>
-      </div>
+      <p>
+        <input type="search" value={value} onChange={(e) => setValue(e.target.value)} />
+      </p>
+
+      {/* useMemo - memoriza componentes, sem re-renderizar caso o pai atualize, porem o compomente do useMemo renderiza se as dependencias mudarem */}
+      {useMemo(() => {
+        return (
+          posts.length > 0 &&
+          posts.map((post) => {
+            return <Post key={post.id} post={post} />;
+          })
+        );
+      }, [posts])}
+      {posts.length <= 0 && <p>Ainda não existem posts.</p>}
     </div>
   );
 }
